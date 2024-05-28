763eaaa971853991a301bfea379570b5c79e4d22
AJIT-KUMAR-PANDIT <ajitkumarpandit1999@gmail.com>
2024-05-28 10:33:43 +0530

    "Removed MongoDB connection code"

--- server/index.js
+++ server/index.js
@@ -13,10 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(cors());
 
-// Database connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err));
+
 
 // Routes
 app.get('/', (req, res) => {
